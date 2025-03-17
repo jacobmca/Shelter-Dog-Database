1947952bc5530ec6cac5314e96ba284757e3caba
Jacob McAuliffe <64445624+jacobmca@users.noreply.github.com>
2025-03-17 15:37:39 -0700

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+    allowedHosts: [
+      'shelter-dog-database.onrender.com',
+      '.onrender.com' // This will allow all subdomains on render.com
+    ]
+  }
 })
